de4c9475bd0649f90406c27323cc63a83c5a4358
zatway <radmirar00@mail.ru>
2026-01-18 21:30:28 +0500

    17

---
+++ PZ_17/PZ_17/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace PZ_17
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a <see cref="Frame">.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            button2.Content = "успешно!";
+        }
+    }
+}
---
+++ PZ_17/PZ_17/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PZ_17
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Group { get; set; }
+
+        public override string ToString()
+        {
+            return $"студент {this.Name} из группы {this.Group}";
+        }
+    }
+}
